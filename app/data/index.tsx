637a260627b1e363b5ca3228a194b6b489d85abe
ChineduOscar <chinedunnadoziepatrick@gmail.com>
2026-01-09 00:07:13 +0100

    making an edit to my portfolio

--- app/data/index.tsx
+++ app/data/index.tsx
@@ -27,7 +27,7 @@ interface Project {
 
 export const projects: Project[] = [
   {
-    id: 2,
+    id: 1,
     title: 'LearnQube',
     description:
       'An e-learning platform where users can buy and watch videos to learn tech skills. It offers a wide variety of courses designed to enhance learning through engaging video content.',
@@ -44,7 +44,7 @@ export const projects: Project[] = [
     ],
   },
   {
-    id: 3,
+    id: 2,
     title: 'LancheAI',
     description:
       'A Web3-powered AI system built on the Avalanche blockchain offering decentralized intelligent solutions for business automation. It enhances decision-making, security, and operational efficiency with smart contract integration.',
@@ -61,7 +61,7 @@ export const projects: Project[] = [
     ],
   },
   {
-    id: 4,
+    id: 3,
     title: 'Raymart',
     description:
       'An intuitive e-commerce platform for seamless shopping experiences with product search, secure checkout, and personalized recommendations.',
@@ -78,7 +78,7 @@ export const projects: Project[] = [
     ],
   },
   {
-    id: 1,
+    id: 4,
     title: 'SmartifyPDF',
     description:
       'SmartifyPDF uses AI to instantly convert your PDFs into interactive quizzes, making learning faster and more engaging.',
